3b22a010d9902243ae79fb6c65a8ecf70c6240c5
Diksha Kadam <dikshakadam77199@gmail.com>
2025-10-15 15:36:37 +0530

    feat: Consolidate all homepage sections into Home.js

--- OneDrive/Desktop/Mini project/pawtrack/src/pages/Home.js
+++ OneDrive/Desktop/Mini project/pawtrack/src/pages/Home.js
@@ -35,7 +35,7 @@ const Hero = () => {
               </p>
             </div>
             <div className="flex flex-col sm:flex-row gap-4">
-              <Link to="/report-animal" className="btn btn-primary btn-lg group btn-glow">
+              <Link to="/report" className="btn btn-primary btn-lg group btn-glow">
                 <Camera className="w-5 h-5 mr-2 group-hover:rotate-12 transition-transform" />
                 Report an Animal
                 <ArrowRight className="w-5 h-5 ml-2 group-hover:translate-x-1 transition-transform" />
